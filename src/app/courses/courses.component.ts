b83b397509d7591bbc28378ebf6e363c8dac81aa
Ousmane DIOP <ousmanediopp268@gmail.com>
2020-10-12 13:17:51 +0000

    update course component

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CoursesService } from '../shared/services/courses.service';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-
-  constructor() { }
+  courses = {}
+  selectedCourse = null;
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit(): void {
+    this.loadCourses(true);
+    this.resetCourse();
+  }
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
 
+  loadCourses(displayNotification: boolean) {
+    this.courses = this.coursesService.getAllCourses(displayNotification);
+  }
+
+  saveCourse(course) {
+    if (course.id) {
+      this.updateCourse(course);
+    } else {
+      this.createCourse(course);
+    }
+    this.resetCourse();
+  }
+
+  updateCourse(course) {
+    this.coursesService.updateCourse(course)
+  }
+
+  createCourse(course) {
+    this.coursesService.createCourse(course)
+  }
+
+  deleteCourse(id: number) {
+    this.coursesService.deleteCourse(id)
+  }
+
+  resetCourse() {
+    const emptyCourse = {
+      id: null,
+      title: '',
+      description: '',
+      percentComplete: 0,
+      favorite: false
+    };
+
+    this.selectCourse(emptyCourse);
+  }
 }
--- src/app/shared/services/courses.service.ts
+++ src/app/shared/services/courses.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class CoursesService {
 
   constructor() { }
+  getAllCourses(displayNotification: boolean) {
+    if (displayNotification) {
+      console.log('Get All Courses HTTP Call');
+    }
+    return null;
+  }
+
+  createCourse(course) {
+    console.log("created course", course);
+
+  }
+
+  updateCourse(course) {
+    console.log("update course", course);
+  }
+
+  deleteCourse(id: number) {
+    console.log("deleted course", id);
+  }
 }
