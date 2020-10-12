b12c23812332b37e241929a81f1c9f72a7b0d4cf
Ousmane DIOP <ousmanediopp268@gmail.com>
2020-10-12 12:59:53 +0000

    adding courses component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
 import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
