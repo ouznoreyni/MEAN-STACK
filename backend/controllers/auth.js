661b70edad474703aaf1fffaa7e8b32cdb6d5732
Ousmane DIOP <ousmanediopp268@gmail.com>
2020-10-15 20:37:22 +0000

    update: refactoring and fix some issues in the login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,38 +5,51 @@ const { User } = require('../models/user');
 const generateToken = require('../utils/generateToken');
 
 exports.signup = async (req, res) => {
-    const { error } = validateUser(req.body)
-    if (error) return res.status(400).json({ error: error.details[0].message })
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
     try {
-        const salt = await bcrypt.genSalt(10)
+        const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(req.body.password, salt);
         const user = { ...req.body, password: hash };
         if (req.body.isAdmin) {
             user.isAdmin = req.body.isAdmin;
         }
+
         await User.create(user);
+
         delete user.password;
+
         const token = generateToken(user);
+
         return res.header("x-auth-token", token).json({ user });
+
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-}
+};
+
+
 exports.login = async (req, res) => {
     //validate data
-    const { error } = validateLogin(req.body)
+    const { error } = validateLogin(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message });
-    //get user
-    const { dataValues: user } = await User.findOne({ where: { username: req.body.username } });
-    if (!user) return res.status(404).json({ error: "Utilisateur n'existe pas" });
-    const passwordValid = await bcrypt.compare(req.body.password, user.password);
-    if (!passwordValid) {
-        return res.status(400).json({ error: "login ou mot de passe incorrect" });
-    }
+
+    const userFinded = await User.findOne({ where: { username: req.body.username } });
+    if (!userFinded) return res.status(404).json({ error: "Utilisateur n'existe pas" });
+
+    const passwordValid = await bcrypt.compare(req.body.password, userFinded.password);
+    if (!passwordValid) return res.status(400).json({ error: "login ou mot de passe incorrect" });
+
+    const user = userFinded.dataValues;
     delete user.password;
+
     const token = generateToken(user);
+
     return res.header('x-auth-token', token).json({ user });
-}
+};
+
+
 exports.logout = (req, res) => {
     return res.json({ data: "logou" })
 }
--- backend/controllers/courses.js
+++ backend/controllers/courses.js
@@ -1,78 +1,80 @@
 const { Course } = require('../models/course');
 const { validateCourse } = require('../validations/course');
 
+
 exports.course_all = async (req, res) => {
     const courses = await Course.findAll();
     return res.json({ courses });
 }
 
+
 exports.course_details = async (req, res) => {
     const { course: id } = req.params;
+
     try {
         const course = await Course.findByPk(id);
-        if (!course) {
-            return res.status(404).json({ error: `Error retrieving course with id ${id}` })
-        }
+        if (!course) return res.status(404).json({ error: `Error retrieving course with id ${id}` });
+
         return res.json({ course });
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
-}
+};
+
+
 exports.course_post = async (req, res) => {
-    // Validate request
     const { error } = validateCourse(req.body);
-    if (error) return res.status(400).json({ error: error.details[0].message })
-    // Save course in the database
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+
     try {
         const course = await Course.create({
             title: req.body.title,
             description: req.body.description,
         });
-        res.json({ course });
+
+        return res.status(201).json({ course });
     } catch (error) {
         return res.status(500).json({
-            error:
-                error.message || "Some error occurred while creating the course."
+            error: error.message || "Some error occurred while creating the course."
         });
     }
 
-}
+};
+
+
 exports.course_patch = async (req, res) => {
-    // Validate request
+
     const { error } = validateCourse(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message })
-    //update
+
     try {
         const { course: id } = req.params;
+
         const course = await Course.findByPk(id);
-        if (!course) {
-            return res.status(404).json({ error: `course with id ${id} is not found` })
-        }
-        await Course.update({ ...req.body }, {
-            where: { id }
-        });
+        if (!course) return res.status(404).json({ error: `course with id ${id} is not found` });
+
+        await Course.update({ ...req.body }, { where: { id } });
+
         return res.json({ message: `course with id ${id} is updated` });
     } catch (error) {
         res.status(500).json({
-            error:
-                error.message || "Some error occurred while updating the course."
+            error: error.message || "Some error occurred while updating the course."
         });
     }
-}
+};
+
+
 exports.course_delete = async (req, res) => {
     const { course: id } = req.params;
+
     try {
-        const deleted = await Course.destroy({
-            where: { id }
-        });
-        if (deleted) {
-            return res.json({ message: `course with id ${id} is deleted` });
-        }
-        return res.status(404).json({ error: `Could not delete course with id ${id}` })
+        const deleted = await Course.destroy({ where: { id } });
+        if (deleted) return res.status(204).json({ message: `course with id ${id} is deleted` });
+
+        return res.status(404).json({ error: `Could not delete course with id ${id}` });
     } catch (error) {
-        return res.status(500).json({
-            error: error.message
-        })
+        return res.status(500).json({ error: error.message });
     }
 
-}
+};
