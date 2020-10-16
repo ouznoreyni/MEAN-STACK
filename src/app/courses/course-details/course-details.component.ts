9c2a039d733078f5ebe117747e7ca4c4839bb1bc
Ousmane DIOP <ousmanediopp268@gmail.com>
2020-10-16 17:17:30 +0000

    update:course details

--- src/app/courses/course-details/course-details.component.ts
+++ src/app/courses/course-details/course-details.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.css'],
 })
-export class CourseDetailsComponent implements OnInit {
-  @Input() course;
+export class CourseDetailsComponent {
+  selectedCourse;
+  originalTitle;
   @Output() save = new EventEmitter();
   @Output() cancel = new EventEmitter();
-  constructor() {}
-
-  ngOnInit(): void {}
+  @Input() set course(value) {
+    if (value) {
+      this.selectedCourse = { ...value };
+      this.originalTitle = value.title;
+    }
+  }
 }
